a321aecee8a1af96a61bed75d5c7dde9aa4af5c0
Sahil Shaikh <sahilshaikh3102@gmail.com>
2022-09-25 20:22:30 +0530

    first commit

---
+++ src/data/product_data.js
@@ -0,0 +1,75 @@
+const product_card = [
+    {
+        id: 1,
+        product_name: "Evee 1",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 2,
+        product_name: "Evee beast",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 3,
+        product_name: "Evee thunder",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 4,
+        product_name: "Evee bolt",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 5,
+        product_name: "Evee flash",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 6,
+        product_name: "Evee limited edition",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 7,
+        product_name: "Evee 2",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 8,
+        product_name: "Evee 2.0",
+        description: "Max. Speed 150 Kmph",
+        price:150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    },
+    {
+        id: 9,
+        product_name: "Evee all star",
+        description: "Max. Speed 150 Kmph",
+        price: 150000,
+        currency: "₹",
+        thumb: "https://images.squarespace-cdn.com/content/v1/572b8cae40261d167186e601/1465607563454-XP1MQC1FFYLZU4O9L9QH/Concept+Bike.5.png?format=1000w"
+    }
+]
+export default product_card;

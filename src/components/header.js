c4bbe4e0dacaf280ed25a4fec55ff72ba71442d2
Sahil Shaikh <sahilshaikh3102@gmail.com>
2022-10-03 20:18:12 +0530

    remove nav

--- src/components/header.js
+++ src/components/header.js
@@ -2,19 +2,20 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <nav>
-            <div className="logo">Evee</div>
-            <ul>
-                <li>Home</li>
-                <li>Our Products</li>
-                <li>About Us</li>
-                <li>Contact</li>
-            </ul>
-            <div className="search">
-                <i className="fa fa-search"></i>
-                <i className="fa fa-shopping-basket"></i>
-            </div>
-        </nav>
+        <div/>
+        // <nav>
+        //     <div className="logo">Evee</div>
+        //     <ul>
+        //         <li>Home</li>
+        //         <li>Our Products</li>
+        //         <li>About Us</li>
+        //         <li>Contact</li>
+        //     </ul>
+        //     <div className="search">
+        //         <i className="fa fa-search"></i>
+        //         <i className="fa fa-shopping-basket"></i>
+        //     </div>
+        // </nav>
        
     )
 }
